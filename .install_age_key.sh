ea82f4f70193c18896f6447d2a171f078c99fd43
Dennie de Lange <621098+tkvw@users.noreply.github.com>
2024-08-06 12:59:43 +0200

    changed master key location

--- .install_age_key.sh
+++ .install_age_key.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-AGE_KEY_FILE="~/.config/age/master.key.txt"
+AGE_KEY_FILE=~/.config/age/master.key.txt
 
 if [ -f "$AGE_KEY_FILE" ]; then
+    echo "Key already exists"
     exit 0
 fi
-if [ ! -z "${AGE_KEY}" ]; then
+if [ -z "${AGE_KEY}" ]; then
+    echo "AGE_KEY is not set"
     exit 1
 fi
 
-mkdir -p $(dirname $AGE_KEY_FILE)
+mkdir -p $(dirname "$AGE_KEY_FILE")
 
-echo "${AGE_KEY}" > $AGE_KEY_FILE
+echo "$AGE_KEY" > "$AGE_KEY_FILE"
 
