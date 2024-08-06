55201a06f3ea47f35ee26c3c528aa552394f6a90
Dennie de Lange <621098+tkvw@users.noreply.github.com>
2024-08-06 13:46:51 +0200

    change mode to profile and set default to CHEZMOI_PROFILE

--- .install_age_key.sh
+++ .install_age_key.sh
@@ -6,12 +6,12 @@ if [ -f "$AGE_KEY_FILE" ]; then
     echo "Key already exists"
     exit 0
 fi
-if [ -z "${AGE_KEY}" ]; then
-    echo "AGE_KEY is not set"
+if [ -z "${CHEZMOI_AGE_KEY}" ]; then
+    echo "CHEZMOI_AGE_KEY is not set"
     exit 1
 fi
 
 mkdir -p $(dirname "$AGE_KEY_FILE")
 
-echo "$AGE_KEY" > "$AGE_KEY_FILE"
+echo "$CHEZMOI_AGE_KEY" > "$AGE_KEY_FILE"
 
