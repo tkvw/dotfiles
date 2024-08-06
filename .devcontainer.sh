4455a53ea42f5861efa19218269b424444dd3e18
Dennie de Lange <621098+tkvw@users.noreply.github.com>
2024-08-06 13:34:50 +0200

    set target to docker in devcontainer

--- .devcontainer.sh
+++ .devcontainer.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Installer script for chezmoi in vscode
-sh -c "$(curl -fsLS get.chezmoi.io)" -- init --promptDefaults --apply
+sh -c "$(curl -fsLS get.chezmoi.io)" -- init --promptDefaults --promptChoice target=docker --apply
