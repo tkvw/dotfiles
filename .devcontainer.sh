45b3fb680fcaa2e128b56fa0e4347c88c38da056
Dennie de Lange <621098+tkvw@users.noreply.github.com>
2024-08-07 10:37:08 +0200

    use lb script variant for devcontainer

--- .devcontainer.sh
+++ .devcontainer.sh
@@ -1,18 +1,5 @@
 #!/bin/sh
 
 set -e # -e: exit on error
-
-if [ ! "$(command -v chezmoi)" ]; then
-  bin_dir="$HOME/.local/bin"
-  if [ "$(command -v curl)" ]; then
-    sh -c "$(curl -fsSL get.chezmoi.io)" -- -b "$bin_dir" init --promptDefaults --promptChoice target=docker --apply
-  elif [ "$(command -v wget)" ]; then
-    sh -c "$(wget -qO- get.chezmoi.io)" -- -b "$bin_dir" init --promptDefaults --promptChoice target=docker --apply
-  else
-    echo "To install chezmoi, you must have curl or wget installed." >&2
-    exit 1
-  fi
-fi
-
-# Installer script for chezmoi in vscode
-# sh -c "$(curl -fsLS get.chezmoi.io)" -- init --promptDefaults --promptChoice target=docker --apply
+  
+sh -c "$(curl -fsSL get.chezmoi.io/lb)" -- init --promptDefaults --promptChoice target=docker --apply
