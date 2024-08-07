8122b3d2323dc88b5d903d67add231442cc56ef2
Dennie de Lange <621098+tkvw@users.noreply.github.com>
2024-08-07 11:06:21 +0200

    allow curl and wget to be used to install chezmoi

--- .devcontainer.sh
+++ .devcontainer.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
 set -e # -e: exit on error
-  
-sh -c "$(curl -fsSL get.chezmoi.io/lb)" -- init --promptDefaults --promptChoice target=docker --apply
+
+if [ ! "$(command -v chezmoi)" ]; then
+  bin_dir="$HOME/.local/bin"
+  if [ "$(command -v curl)" ]; then
+    sh -c "$(curl -fsSL get.chezmoi.io)" -- -b "$bin_dir"
+  elif [ "$(command -v wget)" ]; then
+    sh -c "$(wget -qO- get.chezmoi.io)" -- -b "$bin_dir"
+  else
+    echo "To install chezmoi, you must have curl or wget installed." >&2
+    exit 1
+  fi
+fi
+
+chezmoi init --promptDefaults --apply
