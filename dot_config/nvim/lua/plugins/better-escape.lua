84e4b6e18b4899d247abf02e8f54a0902536ac1b
Dennie de Lange <621098+tkvw@users.noreply.github.com>
2026-02-18 16:12:16 +0100

    Dutch uses words like kijken which conflicts

--- dot_config/nvim/lua/plugins/better-escape.lua
+++ dot_config/nvim/lua/plugins/better-escape.lua
@@ -2,7 +2,35 @@ return {
   {
     "max397574/better-escape.nvim",
     opts = {
-
+      mappings = {
+        -- i for insert
+        i = {
+          j = {
+            -- These can all also be functions
+            j = "<Esc>",
+          },
+        },
+        c = {
+          j = {
+            j = "<C-c>",
+          },
+        },
+        t = {
+          j = {
+            j = "<C-\\><C-n>",
+          },
+        },
+        v = {
+          j = {
+            j = "<Esc>",
+          },
+        },
+        s = {
+          j = {
+            j = "<Esc>",
+          },
+        },
+      },
     }
   }
 }
