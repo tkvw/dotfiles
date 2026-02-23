77bc6ed557736c79b3bd56519fdebae69b441783
Dennie de Lange <621098+tkvw@users.noreply.github.com>
2026-02-23 14:17:33 +0100

    added starship-jj refactored configurations

--- dot_config/wezterm/configs/general.lua
+++ dot_config/wezterm/configs/general.lua
@@ -1,17 +1,17 @@
 local platform = require("utils.platform")
 
 local config = {
-	exit_behavior = "CloseOnCleanExit", -- if the shell program exited with a successful status
-	exit_behavior_messaging = "Verbose",
-	status_update_interval = 1000,
-	audible_bell = "Disabled",
-	default_workspace = "default",
-	scrollback_lines = 20000,
-	mux_enable_ssh_agent = false,
+  exit_behavior = "CloseOnCleanExit", -- if the shell program exited with a successful status
+  exit_behavior_messaging = "Verbose",
+  status_update_interval = 1000,
+  audible_bell = "Disabled",
+  default_workspace = "default",
+  scrollback_lines = 20000,
+  mux_enable_ssh_agent = false,
 }
 
 if platform.is_windows then
-	config.use_dead_keys = true
+  config.use_dead_keys = false
 end
 
 return config
--- dot_config/wezterm/configs/launch.lua
+++ dot_config/wezterm/configs/launch.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local platform = require("utils.platform")
 
 local config = {
-	default_prog = { "nu" },
+  default_prog = { "mise", "x", "--", "nu" },
 }
 
 return config
